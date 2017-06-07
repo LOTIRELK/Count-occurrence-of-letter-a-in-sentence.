3e042d1f22981c7a6842f0d4eeaffc925c1f66ec
LOTIRELK <lorcan.james.otoole@gmail.com>
2017-06-07 12:47:27 +0100

    trghtr
    gtgttrg

---
+++ Question_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_2
+{
+    //a program that prints the index of each occurrence of the letter 'a' int the string
+    //"Happy days are here again"
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            const string happy = "Happy days are here again.";//Instansiate string
+            var chars = happy.ToCharArray();//convert string to character array named chars
+            for (int i =0; i<chars.Length; i++)//loop to scan through the array
+            {
+                if(chars[i]=='a')//if the number of the index is eqaul to 'a'
+                {
+                    Console.WriteLine(i);//output the number of the location in the array
+                }
+            }
+        }
+    }
+}
